4cc5163a2ddb224b766f9f69b782ffdd8ca396f6
Tonmoy kumar Roy <tonmoykumarroy6@gmail.com>
2023-03-09 00:07:37 +0600

    change in navbar style

--- src/components/Shared/Navbar/Navbar.js
+++ src/components/Shared/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import './Navbar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPaw } from '@fortawesome/free-solid-svg-icons';
 import { UserContext } from '../../../App';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     const [loggedInUser, setLoggedInUser] = useContext(UserContext);
@@ -13,15 +13,19 @@ const Navbar = () => {
         sessionStorage.clear();
         setLoggedInUser({});
     }
+    const activeStyle = {
+        color: "white",
+    }
 
 
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-0">
             <div className="container-fluid">
                 <div className="navbar-brand">
-                    <Link className='text-style' to="/">
+
+                    <NavLink className='text-style' to="/">
                         <h1 className="text-center m-0" style={{ fontSize: "1.5em" }} >Pet <FontAwesomeIcon icon={faPaw} /> Care</h1>
-                    </Link>
+                    </NavLink>
                 </div>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -32,32 +36,30 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link ms-3 active" href="/">Home</a>
-                        </li>
-
-                        <li className="nav-item">
-                            <a className="nav-link ms-3" href="/products">Products</a>
-                        </li>
-
-                        <li className="nav-item">
-                            <a className="nav-link ms-3" href="/dashboard">Dashboard</a>
+                            <NavLink exact className={"nav-link ms-3"} activeStyle={activeStyle} to={"/"} >Home</NavLink>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link ms-3" href="/admin">Admin</a>
+                            <NavLink exact className={"nav-link ms-3"} activeStyle={activeStyle} to={"/products"} >Products</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink exact className={"nav-link ms-3"} activeStyle={activeStyle} to={"/dashboard"} >Dashboard</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink exact className={"nav-link ms-3"} activeStyle={activeStyle} to={"/admin"} >Admin</NavLink>
                         </li>
 
                         {/* <li className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
+                            Dropdown
                             </a>
                             <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><hr className="dropdown-divider" /></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                            <li><a className="dropdown-item" href="#">Action</a></li>
+                            <li><a className="dropdown-item" href="#">Another action</a></li>
+                            <li><hr className="dropdown-divider" /></li>
+                            <li><a className="dropdown-item" href="#">Something else here</a></li>
                             </ul>
-                        </li>
-                        <li className="nav-item">
+                            </li>
+                            <li className="nav-item">
                             <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
                         </li> */}
 
